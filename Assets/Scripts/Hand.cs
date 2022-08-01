06b88bfa23fe7b6aa66fe450dfcab7f8b143cbf1
eternity364 <eternity364@gmail.com>
2022-08-01 19:52:34 +0300

    Added empty class Hand

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Rar");
+    }
+}
