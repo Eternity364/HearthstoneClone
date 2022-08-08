aba2dfbb24b83bf201d8649749837f55ba26b88a
eternity364 <eternity364@gmail.com>
2022-08-08 16:01:13 +0300

    Fixed another issue with card rotating and text display

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -7,12 +7,26 @@ public class CardDisplay : MonoBehaviour
     [SerializeField]
     private GameObject cardInHand;
     [SerializeField]
-    private GameObject textCanvas;
+    private GameObject textCanvasGO;
+    [SerializeField]
+    private Canvas textCanvas;
 
 
     void Update()
     {
-        if (cardInHand != null)
-            textCanvas.SetActive(cardInHand.transform.rotation.y > -0.7 && cardInHand.transform.rotation.y < 0.7);
+        TextVisibilityWorkaround();
+    }
+
+    // Workaround to fix card text visibility after rotating card
+    void TextVisibilityWorkaround()
+    {
+        if (cardInHand != null) {
+            Debug.Log(cardInHand.transform.rotation.y);
+            textCanvasGO.SetActive(cardInHand.transform.rotation.y > -0.7 && cardInHand.transform.rotation.y < 0.7);
+            if (cardInHand.transform.rotation.y != 0)
+                textCanvas.sortingLayerName = "1";
+            else
+                textCanvas.sortingLayerName = "Default";
+        }
     }
 }
