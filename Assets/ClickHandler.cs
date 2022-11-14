c19c9166960f4308d1a034939759b40707246751
eternity364 <eternity364@gmail.com>
2022-11-14 09:30:18 +0300

    Added clicking on card

---
+++ Assets/ClickHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour
+{
+    public void Handle()
+    {
+        Debug.Log(this.gameObject.name);
+    }
+}
