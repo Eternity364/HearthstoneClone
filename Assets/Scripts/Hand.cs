bdc90b803246bb9d6267bae363fec88840348d08
eternity364 <eternity364@gmail.com>
2022-11-14 07:56:17 +0300

    Finished cards sorting in Hand

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -9,40 +9,47 @@ public class Hand : MonoBehaviour
     [SerializeField]
     Vector3 positionShift;
     [SerializeField]
+    int sortingTypeThreshold;
+    [SerializeField]
     Vector3 fanSortingStartPosition;
     [SerializeField]
-    float fanSortingAngleShift;
+    float startAngle;
+    [SerializeField]
+    float endAngle;
 
     void Start()
     {
-        SortFan();
+        int lenght = cards.Length;
+        if (lenght > sortingTypeThreshold)
+            SortFan(lenght);
+        else
+            SortLinear(lenght);
     }
 
-    public void SortLinear()
+    public void SortLinear(int lenght)
     {
-        int lenght = cards.Length;
         Vector3 startPosition = (-lenght / 2 + 1) * positionShift;
         if (lenght % 2 == 1)
-            startPosition -= 0.5f * positionShift;
+            startPosition -= positionShift;
 
         for (int i = 0; i < lenght; i++)
         {
             cards[i].transform.position += startPosition + positionShift * i;
-            cards[i].cardDisplay.SetRenderOrder(lenght - i);
+            cards[i].cardDisplay.SetRenderOrder(i);
         }
     }
 
-    public void SortFan()
+    public void SortFan(int lenght)
     {
-        int lenght = cards.Length - 1;
-        float angleShift = lenght;
-        float startAngle = (-lenght / 2 + 0.5f) * fanSortingAngleShift;
-        if (lenght % 2 == 1)
-            startAngle -= 0.5f * fanSortingAngleShift;
+        float fanSortingAngleShift = (endAngle - startAngle) / lenght;
+        float localStartAngle = startAngle;
+        // if (lenght % 2 == 1)
+            // localStartAngle += 0.5f * fanSortingAngleShift;
 
         for (int i = 0; i < lenght; i++)
         {
-            Quaternion rotation = Quaternion.Euler(0, 0, startAngle + (fanSortingAngleShift * i));
+            float angle = localStartAngle + (fanSortingAngleShift * i);
+            Quaternion rotation = Quaternion.Euler(0, 0, angle);
             Quaternion cardRotation = Quaternion.Euler(0, 0, (startAngle + (fanSortingAngleShift * i)) * 0.5f);
             Vector3 position = rotation * fanSortingStartPosition - fanSortingStartPosition;
             cards[i].transform.position += position;
