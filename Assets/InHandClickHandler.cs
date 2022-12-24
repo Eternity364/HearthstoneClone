ab1f832a65c2e8a619718387f63c43ae33b89f7c
eternity364 <eternity364@gmail.com>
2022-12-24 13:17:04 +0300

    Added clicking on box collider

---
+++ Assets/InHandClickHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InHandClickHandler : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Yep");
+    }
+}
