116f60664a04801919935844f22041441dcc0f53
igor.korobka <eternity364@gmail.com>
2023-08-27 16:01:25 +0400

    Added movement in both sides of Axis X

--- Assets/CardPickedRotationManager.cs
+++ Assets/CardPickedRotationManager.cs
@@ -12,10 +12,10 @@ public class CardPickedRotationManager : MonoBehaviour
     private float currentRotation = 0;
     private float speed = 130;
     private float possDiffMax = 0.1f;
-    private float rotationLimit = 60;
+    private float rotationLimit = 50;
     private Vector3 previousPos;
-    private Tweener toZeroTween;
-    float tweenerSpeed = 0.75f;
+    private Tweener toZeroTween = null;
+    float tweenerSpeed = 0.5f;
 
     void Update()
     {
@@ -40,26 +40,41 @@ public class CardPickedRotationManager : MonoBehaviour
     }
 
     public void Rotate(float posDiffX)
-    {
-        if (posDiffX != 0 && posDiffX > 0) {
+    {   
+        
+        //print(toZeroTween == null);
+        if (posDiffX != 0) {
+            bool wasTween = false;
             if (toZeroTween != null) {
+                wasTween = true;
                 toZeroTween.Kill();
-                rotation = currentRotation;
+                if (Math.Sign(posDiffX) == Math.Sign(currentRotation))
+                    rotation = currentRotation;
+                else
+                    CalculateRotation();
             } 
             else 
             {
+                CalculateRotation();
+            }
+
+            void CalculateRotation () {
                 float rotationCoeff = Math.Abs(posDiffX) / possDiffMax;
                 if (rotationCoeff > 1) rotationCoeff = 1;
 
                 float tempRotation = rotationCoeff * rotationLimit;
                 if (tempRotation > Math.Abs(rotation))
-                    rotation = Math.Sign(posDiffX) + tempRotation;
+                    rotation = Math.Sign(posDiffX) * tempRotation;
             }
            
             currentRotation += speed * Time.deltaTime * Math.Sign(posDiffX);
-            if (Math.Abs(currentRotation) > Math.Abs(rotation)) currentRotation = rotation;
+            if (Math.Sign(currentRotation) == Math.Sign(rotation) && Math.Abs(currentRotation) > Math.Abs(rotation))
+                currentRotation = rotation;
 
-        } 
+            if (Math.Abs(currentRotation) > rotationLimit)
+                currentRotation = rotationLimit * Math.Sign(currentRotation);
+
+        }
         else if (toZeroTween == null && currentRotation != 0)
         {   
             // int sign = Math.Sign(currentRotation);
@@ -69,15 +84,19 @@ public class CardPickedRotationManager : MonoBehaviour
             //         currentRotation = 0;
             // }
 
+            //print("current = " + currentRotation);
             void NullTweener() {
                 toZeroTween = null;
             }
             TweenCallback callback = NullTweener;
 
-           toZeroTween = DOTween.To(()=> currentRotation, x=> currentRotation = x, 0, currentRotation / rotationLimit * tweenerSpeed);
+           toZeroTween = DOTween.To(()=> currentRotation, x=> currentRotation = x, 0, Math.Abs(currentRotation) / rotationLimit * tweenerSpeed);
            toZeroTween.SetEase(Ease.OutQuad).OnKill(callback).SetAutoKill(true);
         }
-        print(currentRotation);
+
+        
+        print("current = " + currentRotation);
+        print("rotation = " + rotation);
         angleSetter.Set(new Vector2(0, currentRotation));
     }
 }
