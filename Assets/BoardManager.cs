9cffa2bee474c00f3b110908ccba35d513dbd9ec
igor.korobka <eternity364@gmail.com>
2023-08-29 17:49:20 +0400

    Added placing multiple cards on board

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BoardManager : MonoBehaviour
 {
     [SerializeField]
     CardOnBoardPlacingAnimation placingAnimation;
+    [SerializeField]
+    Transform playerBoardTransform;
     List<Card> placingCards = new List<Card>();
+    List<Card> cardsOnBoard = new List<Card>();
+
+    float positionShift = 0.4f;
+    List<Tweener> sortingTweens;
+
+    void Start () {
+        sortingTweens = new List<Tweener>();
+    }
 
     public void PlaceCard(Card card)
     {
         placingCards.Add(card);
-        card.gameObject.transform.SetParent(this.gameObject.transform);
+        card.gameObject.transform.SetParent(playerBoardTransform);
         Vector3 position = card.transform.position;
         card.transform.localPosition = new Vector3();
         card.IntermediateParent.transform.position = position;
 
-        placingAnimation.Do(card.IntermediateParent.transform);
+        placingAnimation.Do(card.IntermediateParent.transform, playerBoardTransform.position);
+        cardsOnBoard.Add(card);
+        SortCards();
+    }
+
+    public void SortCards() {
+        for (int i = 0; i < sortingTweens.Count; i++)
+        {
+            sortingTweens[i].Kill();
+        }
+        sortingTweens = new List<Tweener>();
+
+        float startPositionX = (-cardsOnBoard.Count / 2 + 0.5f) * positionShift;
+        if (cardsOnBoard.Count % 2 == 1)
+            startPositionX -= positionShift * 0.5f;
+
+        for (int i = 0; i < cardsOnBoard.Count; i++)
+        {
+            //cardsOnBoard[i].transform.localPosition = new Vector3(startPositionX + positionShift * i, cardsOnBoard[i].transform.localPosition.y,  cardsOnBoard[i].transform.localPosition.z);
+            //cardsOnBoard[i].cardDisplay.SetRenderOrder(i);
+            sortingTweens.Add(cardsOnBoard[i].transform.DOMoveX(startPositionX + positionShift * i, 1f).SetEase(Ease.OutQuad));
+        }
     }
 }
--- Assets/CardOnBoardPlacingAnimation.cs
+++ Assets/CardOnBoardPlacingAnimation.cs
@@ -7,15 +7,15 @@ public class CardOnBoardPlacingAnimation : MonoBehaviour
 {
 
     // Update is called once per frame
-    public void Do(Transform transform)
+    public void Do(Transform cardTransform, Vector3 boardPosition)
     {
         Sequence mySequencePosition = DOTween.Sequence();
-        mySequencePosition.Append(transform.DOMove(new Vector3(0, 0, -0.3f), 0.6f).SetEase(Ease.OutQuart));
-        mySequencePosition.Append(transform.DOMove(new Vector3(0, 0, 0.96f), 0.6f).SetEase(Ease.InQuart));
+        mySequencePosition.Append(cardTransform.DOLocalMove(new Vector3(0, 0, -1.2f), 0.6f).SetEase(Ease.OutQuart));
+        mySequencePosition.Append(cardTransform.DOLocalMove(new Vector3(), 0.6f).SetEase(Ease.InQuart));
         
         Sequence mySequenceAngle = DOTween.Sequence();
-        mySequenceAngle.Append(transform.DORotate(new Vector3(), 0.6f).SetEase(Ease.OutQuad));
-        mySequenceAngle.Append(transform.DORotate(new Vector3(-60, 22, 0), 0.5f).SetEase(Ease.InQuad));
-        mySequenceAngle.Append(transform.DORotate(new Vector3(), 0.1f).SetEase(Ease.OutQuad));
+        mySequenceAngle.Append(cardTransform.DOLocalRotate(new Vector3(), 0.6f).SetEase(Ease.OutQuad));
+        mySequenceAngle.Append(cardTransform.DOLocalRotate(new Vector3(-60, 22, 0), 0.5f).SetEase(Ease.InQuad));
+        mySequenceAngle.Append(cardTransform.DOLocalRotate(new Vector3(), 0.1f).SetEase(Ease.OutQuad));
     }
 }
