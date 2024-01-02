763fff1f13f91ce021f8759380f534df106ad24f
igor.korobka <eternity364@gmail.com>
2024-01-03 02:11:48 +0300

    Made unity server hosting work

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
-using Unity.Netcode;
 using UnityEngine.UI;
+//using ParrelSync;
+using Unity.Services.Multiplay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,13 +14,115 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button startServer;
     [SerializeField] private GameObject serverStartedText;
 
-    private void Start() {
+#if DEDICATED_SERVER
+    private float autoAllocateTimer = 9999999f;
+    private bool alreadyAutoAllocated;
+    private static IServerQueryHandler serverQueryHandler; // static so it doesn't get destroyed when this object is destroyed
+#endif
+
+
+    private void Awake() {
+        
+        Debug.Log("MAIN MENU AWAKE");
+        DontDestroyOnLoad(gameObject);
+
+        InitializeUnityAuthentication();        
+
+#if !DEDICATED_SERVER
+    //if (!ClonesManager.IsClone()) {
         startClient.onClick.AddListener(() => {
             StartClient();
         });
         startServer.onClick.AddListener(() => {
             StartServer();
         });
+    //}
+    //else
+        //StartServer();
+#endif
+    }
+
+#if DEDICATED_SERVER
+    private void MultiplayEventCallbacks_SubscriptionStateChanged(MultiplayServerSubscriptionState obj) {
+        Debug.Log("DEDICATED_SERVER MultiplayEventCallbacks_SubscriptionStateChanged");
+        Debug.Log(obj);
+    }
+
+    private void MultiplayEventCallbacks_Error(MultiplayError obj) {
+        Debug.Log("DEDICATED_SERVER MultiplayEventCallbacks_Error");
+        Debug.Log(obj.Reason);
+    }
+
+    private void MultiplayEventCallbacks_Deallocate(MultiplayDeallocation obj) {
+        Debug.Log("DEDICATED_SERVER MultiplayEventCallbacks_Deallocate");
+    }
+
+    private void MultiplayEventCallbacks_Allocate(MultiplayAllocation obj) {
+        Debug.Log("DEDICATED_SERVER MultiplayEventCallbacks_Allocate");
+
+        if (alreadyAutoAllocated) {
+            Debug.Log("Already auto allocated!");
+            return;
+        }
+
+        alreadyAutoAllocated = true;
+
+        var serverConfig = MultiplayService.Instance.ServerConfig;
+        Debug.Log($"Server ID[{serverConfig.ServerId}]");
+        Debug.Log($"AllocationID[{serverConfig.AllocationId}]");
+        Debug.Log($"Port[{serverConfig.Port}]");
+        Debug.Log($"QueryPort[{serverConfig.QueryPort}]");
+        Debug.Log($"LogDirectory[{serverConfig.ServerLogDirectory}]");
+
+        string ipv4Address = "0.0.0.0";
+        ushort port = serverConfig.Port;
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipv4Address, port, "0.0.0.0");
+
+        StartServer();
+    }
+#endif
+
+    private async void InitializeUnityAuthentication() {
+        if (UnityServices.State != ServicesInitializationState.Initialized) {
+            InitializationOptions initializationOptions = new InitializationOptions();
+            //initializationOptions.SetProfile(UnityEngine.Random.Range(0, 10000).ToString());
+
+            await UnityServices.InitializeAsync(initializationOptions);
+
+#if !DEDICATED_SERVER
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+#endif
+
+#if DEDICATED_SERVER
+            Debug.Log("DEDICATED_SERVER LOBBY");
+
+            MultiplayEventCallbacks multiplayEventCallbacks = new MultiplayEventCallbacks();
+            multiplayEventCallbacks.Allocate += MultiplayEventCallbacks_Allocate;
+            multiplayEventCallbacks.Deallocate += MultiplayEventCallbacks_Deallocate;
+            multiplayEventCallbacks.Error += MultiplayEventCallbacks_Error;
+            multiplayEventCallbacks.SubscriptionStateChanged += MultiplayEventCallbacks_SubscriptionStateChanged;
+            IServerEvents serverEvents = await MultiplayService.Instance.SubscribeToServerEventsAsync(multiplayEventCallbacks);
+
+            serverQueryHandler = await MultiplayService.Instance.StartServerQueryHandlerAsync(50, "MyServer", "HearthstoneClone", "0.1", "Default");
+
+            var serverConfig = MultiplayService.Instance.ServerConfig;
+            if (serverConfig.AllocationId != "") {
+                // Already Allocated
+                MultiplayEventCallbacks_Allocate(new MultiplayAllocation("", serverConfig.ServerId, serverConfig.AllocationId));
+            }
+#endif
+        } else {
+            // Already Initialized
+#if DEDICATED_SERVER
+            Debug.Log("DEDICATED_SERVER LOBBY - ALREADY INIT");
+
+            var serverConfig = MultiplayService.Instance.ServerConfig;
+            if (serverConfig.AllocationId != "") {
+                // Already Allocated
+                MultiplayEventCallbacks_Allocate(new MultiplayAllocation("", serverConfig.ServerId, serverConfig.AllocationId));
+            }
+#endif
+        }
     }
 
     private void StartClient()
@@ -30,6 +137,28 @@ public class MainMenu : MonoBehaviour
         serverStartedText.SetActive(true);
         startClient.gameObject.SetActive(false);
         startServer.gameObject.SetActive(false);
+
+#if DEDICATED_SERVER
+        Debug.Log("Hearthstone Dedicated Server");
+#endif
         NetworkManager.Singleton.StartServer();
+    }
+
+    private void Update() {
+
+#if DEDICATED_SERVER
+        autoAllocateTimer -= Time.deltaTime;
+        if (autoAllocateTimer <= 0f) {
+            autoAllocateTimer = 999f;
+            MultiplayEventCallbacks_Allocate(null);
+        }
+
+        if (serverQueryHandler != null) {
+            if (NetworkManager.Singleton.IsServer) {
+                serverQueryHandler.CurrentPlayers = (ushort)NetworkManager.Singleton.ConnectedClientsIds.Count;
+            }
+            serverQueryHandler.UpdateServerCheck();
+        }
+#endif
     }
 }
